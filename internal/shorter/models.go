fd3b8bec474794577536ebbe8db5eca93e0d8d6a
yaroslav.herasymets <yaroslav.herasymets@gmail.com>
2023-11-14 23:04:40 +0200

    reneme file

--- internal/shorter/models.go
+++
@@ -1,9 +0,0 @@
-package shorter
-
-import "time"
-
-type Link struct {
-	FullLink  string    `gorm:"column:full_link"`
-	Alias     string    `gorm:"column:alias"`
-	CreatedAt time.Time `gorm:"column:created_at"`
-}
